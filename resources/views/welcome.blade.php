72ab2c09a801147cc1b725dbb379ef71388e494a
itsnelsonvargas <75910894+itsnelsonvargas@users.noreply.github.com>
2025-04-20 23:25:29 +0800

    Added a navbar.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,26 +16,45 @@
     </head>
 
     <body class="antialiased">
-    
+
+    <nav class="navbar w-100 navbar-expand-lg navbar-light bg-light px-4">
+        <a class="navbar-brand" href="#">ShortSight</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto gap-3">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+
     <div class="container-fluid">
  
-
+        
         <div class="row">
             <div class="col h-25">
 
-                <h1 class="text-center p-5">URL Shortener</h1>
+                <h1 class="text-center p-5">{{ env('APP_NAME') }}</h1>
 
             </div>
         </div>
 
-
-        <div class="row">
-            <div class="col-md-12 ">
-                
-            </div>
-        </div>  
-
-        <hr style="background-color:white;">
+ 
 
         <div class="row">
 
